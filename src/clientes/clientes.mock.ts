fd40c3b26deff91e6bccf6f5c6a398d05bc7f222
zWesleyDavid <wesleydavid1a@gmail.com>
2025-06-02 17:48:09 -0300

    Configuração inicial

---
+++ src/clientes/clientes.mock.ts
@@ -0,0 +1,52 @@
+import { CreateClienteDto } from './dto/create-cliente.dto';
+
+export const clientesMock: CreateClienteDto[] = [
+    {
+        idade: 30,
+        formacaoEducacional: 'Ensino Superior Completo',
+        estado: 'SP',
+        regiao: 'Sudeste',
+        scoreSerasa: 750,
+        ticketMedio: 200
+    },
+    {
+        idade: 25,
+        formacaoEducacional: 'Ensino Médio Completo',
+        estado: 'RJ',
+        regiao: 'Sudeste',
+        scoreSerasa: 680,
+        ticketMedio: 150
+    },
+    {
+        idade: 40,
+        formacaoEducacional: 'Pós-Graduação',
+        estado: 'MG',
+        regiao: 'Sudeste',
+        scoreSerasa: 800,
+        ticketMedio: 300
+    },
+    {
+        idade: 35,
+        formacaoEducacional: 'Ensino Superior Incompleto',
+        estado: 'ES',
+        regiao: 'Sudeste',
+        scoreSerasa: 720,
+        ticketMedio: 180
+    },
+    {
+        idade: 28,
+        formacaoEducacional: 'Ensino Médio Completo',
+        estado: 'SP',
+        regiao: 'Sudeste',
+        scoreSerasa: 690,
+        ticketMedio: 160
+    },
+    {
+        idade: 32,
+        formacaoEducacional: 'Ensino Superior Completo',
+        estado: 'RJ',
+        regiao: 'Sudeste',
+        scoreSerasa: 770,
+        ticketMedio: 220
+    }
+]
